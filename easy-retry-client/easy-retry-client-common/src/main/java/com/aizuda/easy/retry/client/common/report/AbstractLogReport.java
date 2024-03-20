8df9755d9f75ed25017abb144fbb43425810919f
byteblogs168 <598092184@qq.com>
2024-03-20 23:14:16 +0800

    feat: 3.2.0 1. 优化启动类，避免重复加载

---
+++ easy-retry-client/easy-retry-client-common/src/main/java/com/aizuda/easy/retry/client/common/report/AbstractLogReport.java
@@ -0,0 +1,15 @@
+package com.aizuda.easy.retry.client.common.report;
+
+import com.aizuda.easy.retry.common.log.dto.LogContentDTO;
+
+/**
+ * @author xiaowoniu
+ * @date 2024-03-20 22:56:53
+ * @since 3.2.0
+ */
+public abstract class AbstractLogReport implements LogReport{
+    @Override
+    public void report(LogContentDTO logContentDTO) {
+
+    }
+}
